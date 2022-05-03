8f4a3a6443470c109d3e223787c124d8fa0b33e9
Tuan Nguyen <tuan@atndev.com>
2022-05-03 10:47:11 -0500

    add example solution

---
+++ servoyworldexample/forms/sw.js
@@ -0,0 +1,11 @@
+
+/**
+ * @param {JSEvent} event
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"92A447B5-B478-4CFF-AE0B-48BC4B840CCE"}
+ */
+function click(event) {
+	application.output('clicked!')
+}
