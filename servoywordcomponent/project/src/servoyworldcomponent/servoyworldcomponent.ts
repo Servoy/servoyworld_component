bb6befcc0df0ecb2559ff85a04964d49a65b0c05
Johan Compagner <jcompagner@servoy.com>
2022-05-03 11:21:23 +0200

    sample of a @ViewChild (get a reference to the button dom element in the ts file)
    use that to set a styleclass in the svyNgOnchanges (to be able to react
    at runtime)
    and use it in the svyOnInit to set the click handler after testing if it
    is there.

--- servoywordcomponent/project/src/servoyworldcomponent/servoyworldcomponent.ts
+++ servoywordcomponent/project/src/servoyworldcomponent/servoyworldcomponent.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges, Renderer2, ChangeDetectorRef, EventEmitter, Output } from '@angular/core';
+import { Component, Input, SimpleChanges, Renderer2, ChangeDetectorRef, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 // servoy public is the interface between the component and the Titanium NGClient runtime.
 import { ServoyBaseComponent, IValuelist } from '@servoy/public';
 
@@ -26,6 +26,9 @@ export class Servoyworldcomponent extends ServoyBaseComponent<HTMLDivElement>{
 // an @Output emitter needs to be created based on naming convention
    @Output() valueChange = new EventEmitter();
    
+// this is a field that is a View element pointing to the '#button' dom element, so you have direct access to this in code   
+   @ViewChild('button') button: ElementRef<HTMLButtonElement>;
+   
     constructor(protected readonly renderer: Renderer2, protected cdRef: ChangeDetectorRef) {
          super(renderer, cdRef);
     }
@@ -34,14 +37,29 @@ export class Servoyworldcomponent extends ServoyBaseComponent<HTMLDivElement>{
         // this method is called once when a component is first initialized and the primary DOM element is created.
         // will not be called if there is no #element on a (main) tag, because it will not be able to full initialize then.
         super.svyOnInit();
-    }
+        
+        // now look if an click event handler is set (this is a design time property, can only be set once at init so no watch is needed)
+        if (this.click) {
+            // use the base components renderer to register for the 'click' event our click handler 
+            this.getRenderer().listen(this.button.nativeElement, 'click', this.click);
+        }
+        
+     }
     
     svyOnChanges( changes: SimpleChanges ) {
         // This method is called when an input property above gets a change
         super.svyOnChanges(changes);
+        
+        // if this is an incoming server side change for the button styleclass, 
+        // use the renderer (provided by ServoyBaseComponent) to add this class to the native dom element through the button element ref
+        // this buttonstyleclass is a runtime property so can change at anytime in servoy code (elements.servoyworldcomponent.buttonstyleclass = 'something')
+        // so we need  to watch it through svyOnChanges which is always called if an input changes
+        if (changes['buttonstyleclass']) {
+            this.getRenderer().addClass(this.button.nativeElement, this.buttonstyleclass);
+        }
     }
     
-    valueChanged(data) { 
+    valueChanged(data: any) { 
         this.value = data;
         this.valueChange.emit(this.value);
     }
